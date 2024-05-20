d6b50b38f4951de66423eafcce89544a173c55e2
Jorman Urbina <jormanurbina1@gmail.com>
2024-05-20 12:19:04 +0200

    Se aplican respuestas para el pla 5

---
+++ pla5/pla5Cifo.js
@@ -0,0 +1,186 @@
+ /** ---------------------------------------------------------------------------
+ * Desenvolupament Web Frontend
+ * PLA 5: Fonaments de JavaScript
+ * Mòdul: 2
+ * Dedicació: 5 dies (25 hores)
+ * Data d'entrega: 27 de maig de 2024
+ * ------------------------------------------------------------------------- */
+
+/* Task 1 ------------------------------------------------------------------ */
+const question = "What is the answer to life, universe and everything?";
+let answer = 0;
+answer = 42;
+
+// Write all your variables below this line:
+const cifo = "CIFO";
+const pi = 3.1416;
+let distance = 0;
+distance = answer * 1000 + 195;
+const yeap = true;
+const nope = false;
+let condition = cifo === question;
+condition = distance >= 42000;
+const pi_sign = "π";
+const pi_text = "The number π is a mathematical constant, approximately equal to 3.1416, defined in "+ "the ratio between a circle's circumference and its diameter, the symbol is " + pi_sign;
+const cifo_text = `El Bruc is approximately ${distance} meters away from the ${cifo} location.`
+console.log(cifo_text);
+/* Task 2 ------------------------------------------------------------------ */
+
+const lang = "ca";
+const age = 38;
+
+// Write your first `if` block below this line:
+if(age >=30){console.log("You are a boomer!")} 
+
+// Write your second `if` block below this line:
+
+
+// Write your `switch` block below this line:
+
+// Replace `null` in the line below with your your one-liner using a ternary operator:
+console.log(null);
+
+/* Task 3 ------------------------------------------------------------------ */
+
+let num = 9;
+let sum = 0;
+let product = 1;
+
+// Write your `for` loop below this line:
+
+// Write your `while` loop below this line:
+
+// Write your `do...while` loop below this line:
+
+/* Task 4 ------------------------------------------------------------------ */
+
+function func1() {
+  return n + 10;
+}
+
+function func2(n) {
+  n = n * n;
+}
+
+function func3() {}
+
+function func4(n) {
+  n = n * 42;
+  let m = n / 10;
+  m = m + " is a pretty big number";
+  return n;
+}
+
+function func5() {
+  return "First we take " + city1 + " then we take " + city2;
+}
+
+function fumc6(n) {
+  n = "Double of " + n + " is " + n * 2;
+  return n;
+}
+
+// Do not modify anything in the console.log lines below!
+// But uncomment them to check your results.
+
+// expected 23, got error :(
+// console.log(func1(13));
+
+// expected 25, got undefined :(
+// console.log(func2(5));
+
+// expected 'Hello Mike from London', got undefined :(
+// console.log(func3('Mike', 'London'));
+
+// expected '63 is a pretty big number', got number 630 :(
+// console.log(func4(15));
+
+// expected 'First we take Manhattan then we take Berlin', got error :(
+// console.log(func5('Manhattan', 'Berlin'));
+
+// expected 'Double of 7 is 14', got error :(
+// console.log(func6(7));
+
+/* Task 5 ------------------------------------------------------------------ */
+
+const numbers = [1, 2, 3, 4, 5];
+const days = new Array(
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+);
+const mixed = [
+  Date.now(),
+  "New York",
+  6.62607004,
+  [["Paris", "Rome"]],
+  { a: 97, e: 101, i: 105 },
+  numbers,
+];
+const years = [1978, 2021, 1657, 2301, 1867, 2008];
+
+// Replace the `null`s in the code below with your one-liners:
+console.log(null);
+console.log(null);
+console.log(null);
+console.log(null);
+console.log(null);
+
+// Replace the `null`s in the code below (do not add any new line):
+const tuesdayToFriday = null;
+console.log(tuesdayToFriday);
+const sortedDays = null;
+console.log(sortedDays);
+const number = null;
+console.log(number);
+const maxYear = null;
+console.log(maxYear);
+
+// Today is Friday
+// Today is Monday
+// Today is Saturday
+// Today is Sunday
+// Today is Thursday
+// Today is Tuesday
+// Today is Wednesday
+// Write your `for` loop below this line:
+
+/* Task 6 ------------------------------------------------------------------ */
+
+const laptop = {
+  brand: "Apple",
+  model: "MacBook Pro",
+  os: "macOS Sonoma",
+  year: 2022,
+  memory: "16 GB",
+  processor: "3 GHz Dual-Core Intel Core i7",
+  apps: ["Visual Studio Code", "Xcode", "Dropbox", "Magnet"],
+  owner: {
+    name: "Albert",
+    phone: "678901234",
+  },
+};
+
+// Replace the `null`s in the code below with your one-liners:
+console.log(null);
+console.log(null);
+console.log(null);
+console.log(null);
+
+// Write your code to change some properties below this line:
+
+// brand property has value Apple
+// model property has value MacBook Pro
+// os property has value macOS Sonoma
+// year property has value 2022
+// memory property has value 32 GB
+// processor property has value 3 GHz Dual-Core Intel Core i7
+// apps property is an array with 5 elements
+// owner property is an object with 2 fields
+// Write your `for` loop below this line:
+
+/* Task 7 ------------------------------------------------------------------ */
