f064ee7f24af97508ae9eabd4431e10c36de094b
Jorman Urbina <jormanurbina1@gmail.com>
2024-05-27 10:59:26 +0200

    Actualiza index.html y main.js:
    index.html:
    - Añadido enlace a archivo CSS (styles.css).
    - Modificado input de monto a tipo número.
    - Reordenadas las columnas de la tabla.
    
    main.js:
    - Reestructurado y optimizado el código:
      - Función addCeldaAFila ahora acepta un parámetro opcional para agregar una clase específica.
      - Función cargarDatos ahora ordena los datos alfabéticamente por nombre común del país.
      - Actualización de montos ahora muestra las tarifas de divisas desde un archivo JSON.
      - Eliminado código comentado y no utilizado.

--- Proyecto Final/Comparador de paises/main.js
+++ Proyecto Final/Comparador de paises/main.js
@@ -1,14 +1,14 @@
-const templateRow = document.getElementById("template-row").content;
+/*const templateRow = document.getElementById("template-row").content;
 const tbody = document.querySelector("tbody");
 
-function addCeldaAFila(text, tr) {
-  const td = document.createElement("td");
-  td.textContent = text; //todas las columnas equivalen a 1 fila
-  td.classList.add("py-2");
-  tr.appendChild(td);
-}
-
 function mostrarFilas(params) {
+  function addCeldaAFila(text, tr) {
+    const td = document.createElement("td");
+    td.textContent = text; //todas las columnas equivalen a 1 fila
+    td.classList.add("py-2");
+    tr.appendChild(td);
+  }
+
   for (let i = 1; i <= 10; i++) {
     const tr = document.createElement("tr");
     addCeldaAFila(`soy la culumna ${0} fila ${i}`, tr); //todo esto es una fila
@@ -20,19 +20,79 @@ function mostrarFilas(params) {
     tbody.appendChild(tr);
   }
 }
-
 mostrarFilas();
+*/
+/*async function cargarDatos(params) {
+    const res = await fetch("data.json")
+    const data = await res.json();
+    const divisa =[] 
+    for( const moneda of data){
+        if (Object.keys(moneda.currencies)[0]) {
+            console.log(moneda.name.common,Object.keys(moneda.currencies)[0]);
+        } else {
+            console.log("no tiene currenci");
+        }
+        
+  
+}
+}
+cargarDatos()*/
+const templateRow = document.getElementById("template-row").content;
+const tbody = document.querySelector("tbody");
+const inputMonto = document.querySelector("#monto");
 
-function miFunction(params) {
-  for (let i = 0; i < 5; i++) {
-    const newRow = templateRow.cloneNode(true);
-    const cells = newRow.getElementsByTagName("td");
-    cells[0].textContent = i; //fila 1
-    cells[1].textContent = i; //fila 2
-    cells[2].textContent = i;
-    cells[3].textContent = i;
-    cells[4].textContent = i;
-    newRow.removeAttribute("id");
-    tbody.appendChild(newRow);
+function addCeldaAFila(text, tr, moneda) {
+  const td = document.createElement("td");
+  td.textContent = text; //todas las columnas equivalen a 1 fila
+  td.classList.add("py-2");
+  if (moneda) {
+    td.classList.add(moneda);
+  }
+  tr.appendChild(td);
+}
+async function cargarDatos() {
+  const res = await fetch("paises.json");
+  const data = await res.json();
+  const dataOrdenada = [...data].sort((a, b) => {
+    if (a.name.common < b.name.common) {
+      return -1;
+    }
+    if (a.name.common > b.name.common) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const divisa = [];
+
+  for (const moneda of dataOrdenada) {
+    if (moneda.currencies) {
+      const tr = document.createElement("tr");
+      addCeldaAFila(1, tr, Object.keys(moneda.currencies)[0]);
+      addCeldaAFila(Object.keys(moneda.currencies)[0], tr);
+      addCeldaAFila(moneda.name.common, tr);
+      addCeldaAFila(Object.values(moneda.languages)[0], tr);
+      addCeldaAFila("bandera", tr);
+      tbody.appendChild(tr);
+      divisa.push(moneda);
+      
+    } else {
+      //console.log(moneda.name.common, "No tiene currencies");
+    }
+  }
+
+  actualizarMontos();
+}
+cargarDatos();
+
+async function actualizarMontos() {
+  const res = await fetch("divisas.json");
+  const data = await res.json();
+  const tarifas = data.rates;
+  for (const tarifa in tarifas) {
+    const $tdEuros = document.getElementsByClassName(`${tarifa}`);
+    for (const elemento of $tdEuros) {
+      elemento.textContent = tarifas[tarifa].toFixed(2);
+    }
   }
 }
