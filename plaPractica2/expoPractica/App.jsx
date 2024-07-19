cf21348fb6e2f69113a64ebbc9003331ef295ba7
Jorman Urbina <jormanurbina1@gmail.com>
2024-07-19 18:20:29 +0200

    Se agreagan archivos para aplicacion pla14

--- plaPractica2/expoPractica/App.jsx
+++ plaPractica2/expoPractica/App.jsx
@@ -1,18 +1,30 @@
-import { Text, View, StyleSheet, Image,Button,Alert,TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 
 const App = () => {
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}>Hello world !!!</Text>
-        <Image style={styles.image} source={{uri:"https://picsum.photos/200/200"}}
-         />
-         <TouchableOpacity
-         style={styles.button}
-          onPress={()=>{
-           Alert.alert("hola perri")
-         }}
-         ><Text style={styles.buttonText}>Tocame</Text></TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://picsum.photos/200/200" }}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            Alert.alert("hola perri");
+          }}
+        >
+          <Text style={styles.buttonText}>Tocame</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -25,15 +37,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#292929",
   },
   title: { fontSize: 30, color: "white" },
-  image:{height:200,width:200,borderRadius:100},
-  button:{
-    backgroundColor:"blue",
-    padding:7,
-    marginTop:10,
+  image: { height: 200, width: 200, borderRadius: 100 },
+  button: {
+    backgroundColor: "blue",
+    padding: 7,
+    marginTop: 10,
   },
-  buttonText:{
-    color:"#fff",
-  }
+  buttonText: {
+    color: "#fff",
+  },
 });
 
 export default App;
