f8e9093f52f13550e80a19aa05f796e201058a68
Jorman Urbina <jormanurbina1@gmail.com>
2024-07-19 17:54:20 +0200

    Se agrega archivo para practicar con fazt react native

---
+++ plaPractica2/expoPractica/App.jsx
@@ -0,0 +1,39 @@
+import { Text, View, StyleSheet, Image,Button,Alert,TouchableOpacity } from "react-native";
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world !!!</Text>
+        <Image style={styles.image} source={{uri:"https://picsum.photos/200/200"}}
+         />
+         <TouchableOpacity
+         style={styles.button}
+          onPress={()=>{
+           Alert.alert("hola perri")
+         }}
+         ><Text style={styles.buttonText}>Tocame</Text></TouchableOpacity>
+      </View>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929",
+  },
+  title: { fontSize: 30, color: "white" },
+  image:{height:200,width:200,borderRadius:100},
+  button:{
+    backgroundColor:"blue",
+    padding:7,
+    marginTop:10,
+  },
+  buttonText:{
+    color:"#fff",
+  }
+});
+
+export default App;
